7ed21a312dfbd63f03edd901b407f32db7b4f546
akash7766 <mraakash01932@gmail.com>
2022-10-20 14:47:36 +0600

    navigation changed

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -5,32 +5,16 @@ import Link from "next/link";
 const Navigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
+      <div className="container-fluid d-felx justify-content-between">
         <Link href="newses">
           <a className="navbar-brand">Newses</a>
         </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link href="/photos">
-                <a className="nav-link active" aria-current="page">
-                  Photos
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
+
+        <Link href="/photos">
+          <a className="navbar-brand" aria-current="page">
+            Photos
+          </a>
+        </Link>
       </div>
     </nav>
   );
